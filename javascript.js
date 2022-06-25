19c78076eca4b9c683972d159a9a56f83aa7960a
GeeksMinister <nael_sps@hotmail.com>
2022-06-26 00:41:17 +0200

    Reorginized code & files to suit the new branch

--- javascript.js
+++ javascript.js
@@ -1,14 +1,12 @@
 function checkInput() {
     var name = document.getElementById("name").value;
-    var subject = document.getElementById("subject").value;
-    var phone = document.getElementById("phone").value;
     var email = document.getElementById("email").value;
     var message = document.getElementById("message").value;
-    if (name.trim() == "" || subject.trim() == "" || phone.trim() == "" || email.trim() == "" || message.trim() == "") {
-        alert("Var god och skriv in alla uppgifter !");
+    if (name.trim() == "" || email.trim() == "" || message.trim() == "") {
+        alert("Var god och fill i namn, email, och meddelande !");
     }
     else {
-        alert("Ditt meddelande var skickat till " + email);
+        alert("Ditt meddelande har skickats");
     }
 }
 
